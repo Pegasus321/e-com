b1cb6e0fe8ec45efc62e9ca2b0969379a60eac40
Pegasus321 <ayush.agrawal@vit.edu.in>
2023-09-29 23:23:03 +0530

    working on productListing

--- src/frontend/components/ProductCard/ProductCard.jsx
+++ src/frontend/components/ProductCard/ProductCard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import styles from "./ProductCard.module.css";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 
 export default function ProductCard({ product }) {
   return (
     <article>
-      <div>
+      <div className={styles.imageContainer}>
         <Link to={`/products/${product._id}`}>
           <img src={product.image} alt={product.name} />
         </Link>
--- src/frontend/reducers/FiltersReducer.jsx
+++ src/frontend/reducers/FiltersReducer.jsx
@@ -1,4 +1,5 @@
 export const initialFiltersState = {
   allProducts: [],
   filteredProducts: [],
+  displayableProductsLength: 0,
 };
