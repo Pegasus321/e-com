1f526dfacb4f6b5fc497046f0978bd91ca86c78d
Pegasus321 <ayush.agrawal@vit.edu.in>
2023-10-03 22:44:03 +0530

    working on pagination

--- src/frontend/reducers/FiltersReducer.jsx
+++ src/frontend/reducers/FiltersReducer.jsx
@@ -1,6 +1,18 @@
+import { Filters_Action } from "../utils/actions";
+
 export const initialFiltersState = {
   allProducts: [],
   filteredProducts: [],
   displayableProductsLength: 0,
   paginateIndex: 0,
+};
+
+export const filterReducers = (action, state) => {
+  switch (action.type) {
+    case Filters_Action.UPDATE_PAGINATION:
+      return {
+        ...state,
+        paginateIndex: action.payloadIndex,
+      };
+  }
 };
