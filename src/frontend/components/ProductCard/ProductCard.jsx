a532ae56f7dbdaea2656da9ff1efe053a7568da4
Pegasus321 <ayush.agrawal@vit.edu.in>
2023-10-04 23:42:21 +0530

    working on pagination

--- src/frontend/components/ProductCard/ProductCard.jsx
+++ src/frontend/components/ProductCard/ProductCard.jsx
@@ -8,6 +8,7 @@ export default function ProductCard({ product }) {
       <div className={styles.imageContainer}>
         <Link to={`/products/${product._id}`}>
           <p>{product.name}</p>
+          <p>{product.name}</p>
           <img src={product.image} alt={product.name} />
         </Link>
       </div>
