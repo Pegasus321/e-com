2e9b0b73354e6afe27a8282e5e88c488b7e52592
Pegasus321 <ayush.agrawal@vit.edu.in>
2023-10-07 23:54:46 +0530

    working on productListing

---
+++ src/frontend/components/Error/Error.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Error({ errorText }) {
+  return (
+    <div>
+      <h3>{errorText}</h3>
+      <div className="div">
+        <img src="" alt="" />
+      </div>
+      <Link></Link>
+    </div>
+  );
+}
